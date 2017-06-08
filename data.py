d2e06d77215efd2033bf4c7984aea8959e36d69a
sanprasirt <kuforce@gmail.com>
2017-06-08 10:51:30 +0700

    Simple articles

---
+++ data.py
@@ -0,0 +1,25 @@
+def Articles():
+    articles = [
+        {
+            'id': 1,
+            'title': 'Article One',
+            'body': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim praesentium, dolor magni, rem nemo illum sapiente, soluta molestias alias sint aperiam. Quo quas quod tempore quasi! Id fugiat, excepturi dolorum!',
+            'author': 'Sanprasirt',
+            'create_date': '06-08-2017'
+        },
+         {
+            'id': 2,
+            'title': 'Article Two',
+            'body': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim praesentium, dolor magni, rem nemo illum sapiente, soluta molestias alias sint aperiam. Quo quas quod tempore quasi! Id fugiat, excepturi dolorum!',
+            'author': 'Jonh Doe',
+            'create_date': '06-08-2017'
+        },
+         {
+            'id': 3,
+            'title': 'Article Three',
+            'body': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim praesentium, dolor magni, rem nemo illum sapiente, soluta molestias alias sint aperiam. Quo quas quod tempore quasi! Id fugiat, excepturi dolorum!',
+            'author': 'Brew go',
+            'create_date': '06-08-2017'
+        }
+    ]
+    return articles
